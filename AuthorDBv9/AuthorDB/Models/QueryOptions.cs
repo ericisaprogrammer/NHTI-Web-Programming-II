5a51a6827fecf6ff649421e131fbb035fef1255d
Eric Osgood <ericthegamefreak@gmail.com>
2018-04-02 17:00:34 -0400

    AuthorDB Lab 9

---
+++ AuthorDBv9/AuthorDB/Models/QueryOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthorDB.Models
+{
+    public class QueryOptions
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public string SortField { get; set; }
+        public SortOrder SortOrder { get; set; }
+        public string Sort
+        {
+            get
+            {
+                return string.Format("{0} {1}",
+                    SortField, SortOrder.ToString());
+            }
+        }
+        public QueryOptions()
+        {
+            CurrentPage = 1;
+            PageSize = 5;
+            SortField = "ID";
+            SortOrder = SortOrder.ASC;
+        }
+
+        
+    }
+}
